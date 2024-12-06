8dfa76b9ff05606a3d7262c4b5346bc1cd932f99
NidhiDubey1210 <mais.coding@gmail.com>
2024-12-06 17:52:19 +0530

    variables

---
+++ datatype.js
@@ -0,0 +1,59 @@
+//number
+let num = 123456789;
+console.log(num);
+
+let int = 123456789;
+console.log(int);
+
+
+// string
+let char = "Nishal";
+console.log(char);
+let char1 = "Kushal";
+console.log(char1);
+
+
+// boolean //true, false
+let bool = true;
+console.log(bool)
+let bool1 = false;
+console.log(bool)
+
+// bigInt
+let value = 123098243098324n;
+console.log(value);
+let value1 = 14652328987465165749561268416532489654561n;
+console.log(value);
+
+// null
+let passWord = null;
+console.log(passWord)
+
+// undefined
+
+let color;
+console.log(color)
+
+//not primitive types (reference type)
+// Object
+let object = { 
+    name: "Sara", age:"25" };
+console.log(object.name,object.age)
+console.log(object["name","age"])
+console.log(object)
+
+// Array
+let flower = ["rose","Lily","Sunflower",["1253","4566"]]
+console.log(flower)
+let fruit = ["Mango","Orange","Grapes",["1253","4566"]]
+console.log(fruit)
+
+// function
+//normal function 
+function print(){
+  console.log("Zydus School for Excellence")
+}
+print()
+{
+    
+}
